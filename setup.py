e7fbb3b33fc1f815fa6f18a9115a0859588759ed
Eric Moritz <eric@themoritzfamily.com>
2014-05-21 22:13:27 +0000

    Added a dependency link to enable the installation of rdflib-jsonld

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     install_requires=[
         "rdflib==4.1.2",
         "click==0.7",
+        "rdflib-jsonld",
     ],
     packages=[
         "feature_flags",
@@ -13,4 +14,7 @@ setup(
     scripts=[
         "bin/feature-lint",
     ],
+    dependency_links=[
+        "http://github.com/RDFLib/rdflib-jsonld/tarball/ed3241a116e551a15aa0d9c187e272ce7b636c55#egg=rdflib-jsonld",
+    ]
 )
